edd855170621af20487c2b4857645b9dfc53df66
Abdul Musawir <ap.musawir@gmail.com>
2023-03-07 16:25:34 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
-const express = require("express");
+import express from 'express'
+import connect from './db/mongodb.js';
+import cors from 'cors'
+import bodyParser from 'body-parser';
+import routes from './routes/index.js';
+import passport  from 'passport';
+// import passportConfig from 
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
+app.use(bodyParser.json())
+
+app.use('/', routes)
+
+await connect();
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
